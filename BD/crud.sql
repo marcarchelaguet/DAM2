dd3b5ddb8a1356652d03a6c9f9cbc85d9caffdfd
marcarchelagueti <marcarchelagueti@inspladelestany.cat>
2025-09-29 18:49:42 +0200

    Instruccions sql per crear la base de dades

---
+++ BD/crud.sql
@@ -0,0 +1,10 @@
+-- Crear la base de dades DAM2
+create database DAM2;
+use DAM2;
+
+--Crear la taula Professors
+create table Professors(
+    id int primary key,
+    nom varchar(50) not null,
+    cognoms varchar(50) not null,
+);
